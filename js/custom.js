186902096fabbab82e9c527ed5cdefd745f3bf70
dineshmehra86 <dinesh.mehra86@gmail.com>
2023-06-09 00:13:23 +0530

    Variables naming convention

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// let a; //Decleration
+
+
+
+// We cannot redeclare variable using let keyword 
+
+// let b = 10; //Decleration and Initialization
+
+// let b = 20; //Redecleration and Initialization
+
+
+
+
+
+// We can redeclare variable using var keyword
+var d = 50; // Decleration and Initialization
+var d = 10;  // Redecleration and Initialization
+console.log(d);
+
+
